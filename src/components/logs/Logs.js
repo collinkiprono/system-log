7f2de887ae0e60d3e87930f428dfc6826dabdeb5
Collins Kiprono <one.kcollin@gmail.com>
2022-07-27 12:52:52 +0300

    Initial commit

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import LogItems from "./LogItems";
+import Preloader from "../layout/Preloader";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+  if (loading) {
+    return <Preloader />;
+  }
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No logs to show!</p>
+      ) : (
+        logs.map((log) => <LogItems log={log} key={log.id} />)
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
