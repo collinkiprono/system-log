ad37581f52384bc3a1835414d4a5be63f398ea63
Collins Kiprono <one.kcollin@gmail.com>
2022-07-28 13:25:28 +0300

    Add search feature

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
+  SET_CURRENT,
+  SEARCH_LOGS,
+} from "./types";
 
 // export const getLogs = () => {
 //   return async (dispatch) => {
@@ -13,6 +23,8 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //     });
 //   };
 // };
+
+// Get log items
 export const getLogs = () => async (dispatch) => {
   try {
     setLoading();
@@ -29,6 +41,111 @@ export const getLogs = () => async (dispatch) => {
       payload: err.response.data,
     });
   }
+};
+
+// Add new log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+//Delete log from server
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: id,
+    });
+  }
+};
+
+//Update log from server
+export const updateLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/${log.id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//Search log items
+
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//Set current log
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+// Clear current
+export const clearCurrent = (log) => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 //Set loading to true
--- src/components/logs/LogItems.js
+++ src/components/logs/LogItems.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
+import { connect } from "react-redux";
+import { deleteLog, setCurrent } from "../../actions/logActions";
 
-const LogItems = ({ log }) => {
+import M from "materialize-css/dist/js/materialize.min";
+
+const LogItems = ({ log, deleteLog, setCurrent }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: "Log deleted!" });
+  };
   return (
     <li className="collection-item">
       <div>
@@ -11,6 +19,7 @@ const LogItems = ({ log }) => {
           className={`modal-trigger ${
             log.attention ? "red-text" : "blue-text"
           } `}
+          onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
@@ -20,7 +29,7 @@ const LogItems = ({ log }) => {
           <span className="black-text">{log.tech}</span> on{" "}
           <Moment format="MMMM Do YYYY, h:mm::ss a">{log.date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a href="#!" onClick={onDelete} className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -30,6 +39,8 @@ const LogItems = ({ log }) => {
 
 LogItems.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
-export default LogItems;
+export default connect(null, { deleteLog, setCurrent })(LogItems);
